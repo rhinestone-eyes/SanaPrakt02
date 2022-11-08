68d4dbc422dc44b3beb6398dc0e29d7f8d579807
Dmytro Burylko <sana22_bdv@student.ztu.edu.ua>
2022-11-08 21:11:45 +0200

    second text commit

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -2,5 +2,5 @@
 Console.WriteLine("Dima Burylko");
 Console.WriteLine("Zhytomyr");
 Console.WriteLine("Korostyshiv");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("Sana");
+Console.WriteLine("Commerce");
